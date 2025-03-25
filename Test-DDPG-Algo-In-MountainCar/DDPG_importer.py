982fbd360d7c41feacccd01c8f531c52fe35e8c6
Yanlin Yu <yuyanlin1234@163.com>
2025-03-25 22:17:19 +0800

    modified:   Test-DDPG-Algo-In-MountainCar/DDPG.ipynb 	modified:   Test-DDPG-Algo-In-MountainCar/DDPG_importer.py 	new file:   Test-DDPG-Algo-In-MountainCar/__pycache__/DDPG_importer.cpython-312.pyc 	modified:   Test-DDPG-Algo-In-MountainCar/mountain_car_pyversion.py

--- Test-DDPG-Algo-In-MountainCar/DDPG_importer.py
+++ Test-DDPG-Algo-In-MountainCar/DDPG_importer.py
@@ -1,4 +1,5 @@
 import torch
+import wandb
 from torch import nn
 from torch.nn import functional as F
 import numpy as np
@@ -33,13 +34,16 @@ class Actor(nn.Module):
         # The max value for action
         self.max_action_value = max_action
         self.fc1 = nn.Linear(n_states, n_hiddens)
-        self.fc2 = nn.Linear(n_hiddens, n_actions)
+        self.fc2 = nn.Linear(n_hiddens, 128)
+        self.fc3 = nn.Linear(128, n_actions)
         
     def forward(self, x):
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
-        x = torch.tanh(x)  
+        x = F.leaky_relu(x)
+        x = self.fc3(x)
+        x = F.tanh(x)  
         x = x * self.max_action_value  # 缩放到 [-max_action, max_action]
         return x
     
@@ -49,7 +53,8 @@ class Critic(nn.Module):
     def __init__(self, n_states, n_hiddens, n_actions):
         super(Critic, self).__init__() 
         self.fc1 = nn.Linear(n_states + n_actions, n_hiddens)
-        self.fc2 = nn.Linear(n_hiddens, 1)
+        self.fc2 = nn.Linear(n_hiddens, 128)
+        self.fc3 = nn.Linear(128, 1)
 
     def forward(self, x, a):
         # Combind states and actions togethet
@@ -57,23 +62,29 @@ class Critic(nn.Module):
         x = self.fc1(cat)
         x = F.relu(x)
         x = self.fc2(x)
+        x = F.leaky_relu_(x)
+        x = self.fc3(x)
         return x
     
     
 # DDPG main body
 class DDPG():
     
-    def __init__(self, states_size:int, actions_size:int, max_action:float, batch_size:int, sigma:float, actor_lr:float, critic_lr:float, tau:float, gamma:float, device:str):
+    def __init__(self, states_size:int, actions_size:int, max_action:float, batch_size:int, init_sigma:float, final_sigma:float, actor_lr:float, critic_lr:float, tau:float, gamma:float, device:str):
         self.states_size = states_size
         self.actions_size = actions_size
         self.batch_size = batch_size
         self.max_action = max_action
-        self.sigma = sigma
+        self.init_sigma = init_sigma
+        self.final_sigma = final_sigma
+        self.steps = 0
+        self.sigma = init_sigma
         self.actor_lr = actor_lr
         self.critic_lr = critic_lr
         self.tau = tau
         self.gamma = gamma
         self.device = device
+        self.episodes = 0
         # Actor Net
         self.actor = Actor(states_size, 256, actions_size, max_action).to(device)
         self.actor_target = Actor(states_size, 256, actions_size, max_action).to(device)
@@ -83,15 +94,25 @@ class DDPG():
         # Optimizer
         self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=actor_lr)
         self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=critic_lr)
+        self.actor_target_optimizer = torch.optim.Adam(self.actor_target.parameters(), lr=actor_lr)
+        self.critic_target_optimizer = torch.optim.Adam(self.critic_target.parameters(), lr=critic_lr)
         # Loss Fuction
-        self.loss = nn.MSELoss()
+        self.loss = nn.HuberLoss()
+
+        wandb.log({
+            "Noise Sigma": self.sigma,
+            "Episode": self.steps
+        })
         
     def select_action(self, state):
         state = torch.FloatTensor(state).view(1,-1).to(self.device)
         action = self.actor(state).item()
-        return action + np.random.normal(0, self.sigma, size=self.actions_size)
+        action = action + self.sigma * np.random.randn(self.actions_size)
+        return action
+
     
     def update(self, actor_net, actor_target):
+        #self.sigma = max(self.final_sigma, self.init_sigma - 1e-3 * self.steps)
         for target_param, param in zip(actor_target.parameters(), actor_net.parameters()):
             target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
             
@@ -119,6 +140,14 @@ class DDPG():
         self.actor_optimizer.zero_grad()
         actor_loss.backward()
         self.actor_optimizer.step()
+
+        wandb.log({
+        "Critic Loss": critic_loss,
+        "Actor Loss": actor_loss,
+        "Global Step": self.steps,
+        "Noise Sigma": self.sigma,
+        })
+
         
         self.update(self.actor, self.actor_target)
         self.update(self.critic, self.critic_target)
--- Test-DDPG-Algo-In-MountainCar/mountain_car_pyversion.py
+++ Test-DDPG-Algo-In-MountainCar/mountain_car_pyversion.py
@@ -1,3 +1,5 @@
+
+import wandb
 import torch
 import gymnasium as gym
 import numpy as np
@@ -5,6 +7,18 @@ import matplotlib.pyplot as plt
 from itertools import count
 from DDPG_importer import DDPG, ReplayBuffer
 from matplotlib.animation import FuncAnimation
+
+wandb.init(
+    project="mountain_car_algo_test",  # 项目名称
+    config={                     # 记录超参数
+        "init_sigma":0.5,
+        "final_sigma":0.05,
+        "actor_lr":1e-4,
+        "critic_lr":1e-3,
+        "tau":0.001,
+        "gamma":0.95,
+        "batch_size":128}
+)
 
 # Main body
 print(torch.cuda.is_available())  # 应输出True
@@ -19,36 +33,50 @@ agent = DDPG(states_size=state_size,
              actions_size=action_size,
              max_action=max_action,
              batch_size=128,
-             sigma=0.01,
-             actor_lr=3e-3,
-             critic_lr=3e-3,
-             tau=0.001,
-             gamma=0.9,
+             init_sigma=0.5,
+             final_sigma=0.1,
+             actor_lr=1e-4,
+             critic_lr=1e-3,
+             tau=0.005,
+             gamma=0.99,
              device=device)  # Ensure the device is specified correctly
 replay = ReplayBuffer(capacity=100000)
 
 all_returns = []
 mean_returns = []
 
-for episode in range(500):
+for episode in range(200):
     state = env.reset()[0]
     done = False
     info = False
     episode_return = 0
+    step_count = 0
     while not done:
+        step_count += 1
+        if step_count > 999:
+            print("Break Training")
+            break
         action = agent.select_action(state)
         next_state, reward, done, info, _ = env.step(action)
+        agent.steps += 1
         replay.push(state, action, reward, next_state, done)
         state = next_state
         episode_return += reward
-        if replay.size() > 500:
+        if replay.size() > 1000:
             agent.train(replay)
     all_returns.append(episode_return)
     mean_return = np.mean(all_returns)
     mean_returns.append(mean_return)
+    agent.episodes += 1
+    wandb.log({
+        "Episode Reward": episode_return,
+        "Mean Reward": mean_return,
+        "Episode": episode
+    })
     print("Episode: {}, Return: {}, Mean Return: {}".format(episode, episode_return, mean_return))
 
 env.close()
+wandb.finish()
 
 plt.figure(figsize=(10, 5))
 plt.plot(range(1, len(all_returns) + 1), all_returns, label='Return')
